965714c53575a20acd12680e1545419376f38f08
van015 <ulatvanremlee@gmail.com>
2025-03-21 21:38:07 +0800

    Update and rename userProfile.js to Prob1_userProfile.js

---
+++ CS1A_MTExam_Ulat/Prob1_userProfile.js
@@ -0,0 +1,11 @@
+// Ulat, Van Remlee 
+let fullName = prompt("Enter your full name:");
+let age = prompt("Enter your age:");
+let favoriteNumber = prompt("Enter your favorite number:");
+let favoriteColor = prompt("Enter your favorite color:");
+
+console.log("User Profile:");
+console.log("Full Name:", fullName);
+console.log("Age:", age);
+console.log("Favorite Number:", favoriteNumber);
+console.log("Favorite Color:", favoriteColor);
