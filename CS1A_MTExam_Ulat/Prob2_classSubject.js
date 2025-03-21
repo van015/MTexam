beddf08e5c43a5482058b9e0c94e348d4145e8eb
van015 <ulatvanremlee@gmail.com>
2025-03-21 21:39:52 +0800

    Create Prob2_classSubject.js

---
+++ CS1A_MTExam_Ulat/Prob2_classSubject.js
@@ -0,0 +1,8 @@
+// Ulat, Van Remlee
+let studentName = prompt("Enter your name:");
+let subjectTitle = prompt("Enter the subject title:");
+let classSchedule = prompt("Enter the class schedule (Time, Days):");
+let classroom = prompt("Enter the classroom:");
+let classInstructor = prompt("Enter the class instructor:");
+
+console.log(`${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at room ${classroom}. The instructor for the subject is ${classInstructor}.`);
